10a0f55cd57188826f62a55bc61b5b0c08eee669
Mohammed slimani <slimani.med.21@gmail.com>
2019-01-20 13:04:22 +0000

    Cleaned my pages using beautify
    Clean neat code IS the best after all

--- maquette/captain/addfield.js
+++ maquette/captain/addfield.js
@@ -1,22 +1,23 @@
-$(document).ready(function(){
-    var next = 1;
-    $(".add-more").click(function(e){
-        e.preventDefault();
-        var addto = "#field" + next;
-        next = next + 1;
-        if(next<9){
-          var newIn = '<br /><br /><input autocomplete="off" class="span3" id="field' + next + '" name="field' + next + '" type="text" data-provide="typeahead" data-items="8">';
-          var newInput = $(newIn);
-          $(addto).after(newInput);
-          $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-          $("#count").val(next);
-        }else if(next == 9){/*to show the message only once*/
-          var newIn = '<br /><br /> <div class="span3 alert alert-warning" id="field' + next + '" name="field' + next + '"role="alert" data-provide="typeahead" data-items="8"> You can\'t add anymore fields</div>';
-          var newInput = $(newIn);
-          $(addto).after(newInput);
-          $("#field" + next).attr('data-source',$(addto).attr('data-source'));
-          $("#count").val(next);
-          $("#b1").remove();
-        }
-    });
+$(document).ready(function() {
+  var next = 1;
+  $(".add-more").click(function(e) {
+    e.preventDefault();
+    var addto = "#field" + next;
+    next = next + 1;
+    if (next < 9) {
+      var newIn = '<br /><br /><input autocomplete="off" class="span3" id="field' + next + '" name="field' + next + '" type="text" data-provide="typeahead" data-items="8">';
+      var newInput = $(newIn);
+      $(addto).after(newInput);
+      $("#field" + next).attr('data-source', $(addto).attr('data-source'));
+      $("#count").val(next);
+    } else if (next == 9) {
+      /*to show the message only once*/
+      var newIn = '<br /><br /> <div class="span3 alert alert-warning" id="field' + next + '" name="field' + next + '"role="alert" data-provide="typeahead" data-items="8"> You can\'t add anymore fields</div>';
+      var newInput = $(newIn);
+      $(addto).after(newInput);
+      $("#field" + next).attr('data-source', $(addto).attr('data-source'));
+      $("#count").val(next);
+      $("#b1").remove();
+    }
+  });
 });
